51be0882e25f6507048e6c752b40947808aaeb7e
marcelonfilho <marcelonfilho.ms@gmail.com>
2023-10-18 23:32:52 -0400

    feat: add Catalog page

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -19,7 +19,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
-        <div className="flex flex-col">
+        <div className="flex h-full flex-col">
           <AuthProvider>
             <Header />
             <div className="flex-1">{children}</div>
