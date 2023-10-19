91b0f7fb5504dfcfe26e148b7afab18e2a6d6f9a
marcelonfilho <marcelonfilho.ms@gmail.com>
2023-10-18 21:54:19 -0400

    add: PromoBanner  component and update ProductList component

--- src/app/(home)/components/product-list.tsx
+++ src/app/(home)/components/product-list.tsx
@@ -3,19 +3,26 @@ import computeProductTotalPrice from "@/helpers/products";
 import { Product } from "@prisma/client";
 
 interface ProductListProps {
-  products: Product[]
+  title?: string;
+  products: Product[];
 }
 
-const ProductList = ({ products }: ProductListProps) => {
-  return ( 
-    <div className="flex w-full gap-4 overflow-x-auto [&::-webkit-scrollbar]:hidden px-5">
-      {products.map( product => {
-        return (
-          <ProductItem key={product.id} product={computeProductTotalPrice(product)} />
-        )
-      })}
-    </div>
-   );
-}
- 
+const ProductList = ({ title, products }: ProductListProps) => {
+  return (
+    <>
+      <h1 className="mb-5 px-5 font-bold uppercase">{title}</h1>
+      <div className="flex w-full gap-4 overflow-x-auto px-5 [&::-webkit-scrollbar]:hidden">
+        {products.map((product) => {
+          return (
+            <ProductItem
+              key={product.id}
+              product={computeProductTotalPrice(product)}
+            />
+          );
+        })}
+      </div>
+    </>
+  );
+};
+
 export default ProductList;
