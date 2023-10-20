fedd92d0f8c0f952d84edd23d60f81747a87d8ec
marcelonfilho <marcelonfilho.ms@gmail.com>
2023-10-20 17:46:39 -0400

    feat: ensure cart product can be scrolled

--- src/components/ui/cart.tsx
+++ src/components/ui/cart.tsx
@@ -5,11 +5,13 @@ import { CartContext } from "@/providers/cart";
 import CartItem from "./cart-item";
 import computeProductTotalPrice from "@/helpers/products";
 import { Separator } from "./separator";
+import { ScrollArea } from "./scroll-area";
+import { Button } from "./button";
 
 const Cart = () => {
   const { products, subtotal, total, totalDiscount } = useContext(CartContext);
   return (
-    <div className="flex flex-col gap-8">
+    <div className="flex h-full flex-col gap-8">
       <Badge
         className="w-fit gap-1 border-2 border-primary px-3 py-[0.375rem] text-xs uppercase"
         variant="outline"
@@ -18,52 +20,56 @@ const Cart = () => {
         Carrinho
       </Badge>
 
-      <div className="flex flex-col gap-5">
-        {products.length > 0 ? (
-          products.map((product) => {
-            return (
-              <>
-                <CartItem
-                  key={product.id}
-                  product={computeProductTotalPrice(product as any) as any}
-                />
+      <div className="flex-1 overflow-hidden">
+        <ScrollArea>
+          <div className="flex flex-col gap-5">
+            {products.length > 0 ? (
+              products.map((product) => {
+                return (
+                  <CartItem
+                    key={product.id}
+                    product={computeProductTotalPrice(product as any) as any}
+                  />
+                );
+              })
+            ) : (
+              <p className="text-center text-sm font-semibold opacity-75">
+                Carrinho vazio.
+              </p>
+            )}
+          </div>
+        </ScrollArea>
+      </div>
 
-                <div className="flex flex-col gap-3">
-                  <Separator />
-                  <div className="flex items-center justify-between">
-                    <p className="text-xs">Subtotal</p>
-                    <p className="text-xs">R$ {subtotal.toFixed(2)}</p>
-                  </div>
+      <div className="flex flex-col gap-3">
+        <Separator />
+        <div className="flex items-center justify-between">
+          <p className="text-xs">Subtotal</p>
+          <p className="text-xs">R$ {subtotal.toFixed(2)}</p>
+        </div>
 
-                  <Separator />
+        <Separator />
 
-                  <div className="flex items-center justify-between">
-                    <p className="text-xs">Entrega</p>
-                    <p className="text-xs uppercase">grátis</p>
-                  </div>
+        <div className="flex items-center justify-between">
+          <p className="text-xs">Entrega</p>
+          <p className="text-xs uppercase">grátis</p>
+        </div>
 
-                  <Separator />
+        <Separator />
 
-                  <div className="flex items-center justify-between">
-                    <p className="text-xs">Descontos</p>
-                    <p className="text-xs">- R$ {totalDiscount.toFixed(2)}</p>
-                  </div>
+        <div className="flex items-center justify-between">
+          <p className="text-xs">Descontos</p>
+          <p className="text-xs">- R$ {totalDiscount.toFixed(2)}</p>
+        </div>
 
-                  <Separator />
+        <Separator />
 
-                  <div className="flex items-center justify-between">
-                    <p className="text-sm font-bold">Total</p>
-                    <p className="text-sm font-bold">R$ {total.toFixed(2)}</p>
-                  </div>
-                </div>
-              </>
-            );
-          })
-        ) : (
-          <p className="text-center text-sm font-semibold opacity-75">
-            Carrinho vazio.
-          </p>
-        )}
+        <div className="flex items-center justify-between">
+          <p className="text-sm font-bold">Total</p>
+          <p className="text-sm font-bold">R$ {total.toFixed(2)}</p>
+        </div>
+
+        <Button className="mt-7 font-bold uppercase">Finalizar compra</Button>
       </div>
     </div>
   );
