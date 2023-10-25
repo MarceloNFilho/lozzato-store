9b659e8c565d3ea6ede23c2dd292f679fb0afd62
marcelonfilho <marcelonfilho.ms@gmail.com>
2023-10-24 22:15:57 -0300

    style: make adjust

--- src/app/catalog/page.tsx
+++ src/app/catalog/page.tsx
@@ -8,13 +8,15 @@ const CatalogPage = async () => {
 
   return (
     <div className="flex flex-col items-center gap-8 p-5">
-      <Badge
-        className="w-fit gap-1 border-2 border-primary px-3 py-[0.375rem] text-xs uppercase"
-        variant="outline"
-      >
-        <ShapesIcon size={16} />
-        Catálogo
-      </Badge>
+      <div className="flex w-full items-start">
+        <Badge
+          className="w-fit gap-1 border-2 border-primary px-3 py-[0.375rem] text-xs uppercase"
+          variant="outline"
+        >
+          <ShapesIcon size={16} />
+          Catálogo
+        </Badge>
+      </div>
 
       <div className="grid w-full grid-cols-2 gap-8">
         {categories.map((category) => (
