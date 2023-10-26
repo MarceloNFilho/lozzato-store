112b77d00fc5aa5342a7de99a6269c65a013f8c1
marcelonfilho <marcelonfilho.ms@gmail.com>
2023-10-26 19:13:54 -0300

    style: adjust orders loading

--- src/app/orders/loading.tsx
+++ src/app/orders/loading.tsx
@@ -2,8 +2,8 @@ import LoadingPage from "@/components/ui/loading";
 
 const Loading = () => {
   return (
-    <div className="flex w-full flex-1 flex-col items-center p-5">
-      <div className="flex animate-pulse flex-col gap-8">
+    <div className="flex w-full flex-1 flex-col p-5">
+      <div className="flex w-full animate-pulse flex-col gap-8">
         <div className="h-8 w-28 rounded-lg bg-accent opacity-75" />
         <div className="flex flex-col gap-4">
           <div className="h-12 w-full rounded-lg bg-accent opacity-75" />
