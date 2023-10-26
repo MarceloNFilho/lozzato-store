4ae957bbe30cdfa90fd8527569e94134cabc7b14
marcelonfilho <marcelonfilho.ms@gmail.com>
2023-10-26 19:16:41 -0300

    style: make adjust

--- src/app/orders/loading.tsx
+++ src/app/orders/loading.tsx
@@ -6,13 +6,13 @@ const Loading = () => {
       <div className="flex w-full animate-pulse flex-col gap-8">
         <div className="h-8 w-28 rounded-lg bg-accent opacity-75" />
         <div className="flex flex-col gap-4">
-          <div className="h-12 w-full rounded-lg bg-accent opacity-75" />
-          <div className="h-12 w-full rounded-lg bg-accent opacity-75" />
-          <div className="h-12 w-full rounded-lg bg-accent opacity-75" />
-          <div className="h-12 w-full rounded-lg bg-accent opacity-75" />
-          <div className="h-12 w-full rounded-lg bg-accent opacity-75" />
-          <div className="h-12 w-full rounded-lg bg-accent opacity-75" />
-          <div className="h-12 w-full rounded-lg bg-accent opacity-75" />
+          <div className="h-14 w-full rounded-lg bg-accent opacity-75" />
+          <div className="h-14 w-full rounded-lg bg-accent opacity-75" />
+          <div className="h-14 w-full rounded-lg bg-accent opacity-75" />
+          <div className="h-14 w-full rounded-lg bg-accent opacity-75" />
+          <div className="h-14 w-full rounded-lg bg-accent opacity-75" />
+          <div className="h-14 w-full rounded-lg bg-accent opacity-75" />
+          <div className="h-14 w-full rounded-lg bg-accent opacity-75" />
         </div>
       </div>
     </div>
