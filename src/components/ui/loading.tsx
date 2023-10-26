188a590fbe4f7ba6a938e609503d0256318c899b
marcelonfilho <marcelonfilho.ms@gmail.com>
2023-10-26 18:21:22 -0300

    style: make adjust in the loading component

--- src/components/ui/loading.tsx
+++ src/components/ui/loading.tsx
@@ -2,27 +2,27 @@ const LoadingPage = () => {
   return (
     <div className="flex w-full flex-1 flex-col items-center p-5">
       <div className="flex animate-pulse flex-col gap-8">
-        <div className="h-8 w-28 rounded-lg bg-gray-900" />
+        <div className="h-8 w-28 rounded-lg bg-gray-300 opacity-50" />
         <div className="grid grid-cols-2 gap-8">
           <div className="flex flex-col gap-4">
-            <div className="h-44 w-44 rounded-lg bg-gray-900" />
-            <div className="h-8 w-32 rounded-lg bg-gray-900" />
-            <div className="h-8 w-24 rounded-lg bg-gray-900" />
+            <div className="h-44 w-44 rounded-lg bg-gray-300 opacity-50" />
+            <div className="h-8 w-32 rounded-lg bg-gray-300 opacity-50" />
+            <div className="h-8 w-24 rounded-lg bg-gray-300 opacity-50" />
           </div>
           <div className="flex flex-col gap-4">
-            <div className="h-44 w-44 rounded-lg bg-gray-900" />
-            <div className="h-8 w-32 rounded-lg bg-gray-900" />
-            <div className="h-8 w-24 rounded-lg bg-gray-900" />
+            <div className="h-44 w-44 rounded-lg bg-gray-300 opacity-50" />
+            <div className="h-8 w-32 rounded-lg bg-gray-300 opacity-50" />
+            <div className="h-8 w-24 rounded-lg bg-gray-300 opacity-50" />
           </div>
           <div className="flex flex-col gap-4">
-            <div className="h-44 w-44 rounded-lg bg-gray-900" />
-            <div className="h-8 w-32 rounded-lg bg-gray-900" />
-            <div className="h-8 w-24 rounded-lg bg-gray-900" />
+            <div className="h-44 w-44 rounded-lg bg-gray-300 opacity-50" />
+            <div className="h-8 w-32 rounded-lg bg-gray-300 opacity-50" />
+            <div className="h-8 w-24 rounded-lg bg-gray-300 opacity-50" />
           </div>
           <div className="flex flex-col gap-4">
-            <div className="h-44 w-44 rounded-lg bg-gray-900" />
-            <div className="h-8 w-32 rounded-lg bg-gray-900" />
-            <div className="h-8 w-24 rounded-lg bg-gray-900" />
+            <div className="h-44 w-44 rounded-lg bg-gray-300 opacity-50" />
+            <div className="h-8 w-32 rounded-lg bg-gray-300 opacity-50" />
+            <div className="h-8 w-24 rounded-lg bg-gray-300 opacity-50" />
           </div>
         </div>
       </div>
