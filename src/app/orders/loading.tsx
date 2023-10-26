2911efeae6c58e30388ac3c6c130fea9926461a5
marcelonfilho <marcelonfilho.ms@gmail.com>
2023-10-26 19:04:57 -0300

    style: create loading to orders page

--- src/app/orders/loading.tsx
+++ src/app/orders/loading.tsx
@@ -1,7 +1,22 @@
 import LoadingPage from "@/components/ui/loading";
 
 const Loading = () => {
-  return <LoadingPage />;
+  return (
+    <div className="flex w-full flex-1 flex-col items-center p-5">
+      <div className="flex animate-pulse flex-col gap-8">
+        <div className="h-8 w-28 rounded-lg bg-accent opacity-75" />
+        <div className="flex flex-col gap-4">
+          <div className="h-12 w-full rounded-lg bg-accent opacity-75" />
+          <div className="h-12 w-full rounded-lg bg-accent opacity-75" />
+          <div className="h-12 w-full rounded-lg bg-accent opacity-75" />
+          <div className="h-12 w-full rounded-lg bg-accent opacity-75" />
+          <div className="h-12 w-full rounded-lg bg-accent opacity-75" />
+          <div className="h-12 w-full rounded-lg bg-accent opacity-75" />
+          <div className="h-12 w-full rounded-lg bg-accent opacity-75" />
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Loading;
