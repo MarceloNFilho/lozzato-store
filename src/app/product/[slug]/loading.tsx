bba9b5e346f22234b9d6072d3108bfe092ec49ca
marcelonfilho <marcelonfilho.ms@gmail.com>
2023-10-26 18:44:20 -0300

    style: add loading to product page

--- src/app/product/[slug]/loading.tsx
+++ src/app/product/[slug]/loading.tsx
@@ -1,7 +1,24 @@
-import LoadingPage from "@/components/ui/loading";
-
 const Loading = () => {
-  return <LoadingPage />;
+  return (
+    <div className="flex w-full flex-1 animate-pulse flex-col items-center gap-8">
+      <div className="h-96 w-full bg-accent opacity-75" />
+      <div className="flex flex-col gap-8">
+        <div className="grid grid-cols-4 gap-4">
+          <div className="h-20 w-20 bg-accent opacity-75" />
+          <div className="h-20 w-20 bg-accent opacity-75" />
+          <div className="h-20 w-20 bg-accent opacity-75" />
+          <div className="h-20 w-20 bg-accent opacity-75" />
+        </div>
+
+        <div className="flex flex-col gap-1">
+          <div className="h-6 w-32" />
+          <div className="h-8 w-32" />
+          <div className="h-8 w-32" />
+          <div className="h-10 w-24" />
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Loading;
