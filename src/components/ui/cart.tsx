9a608e388f54f21a530dfba51018ebae32b9bc13
marcelonfilho <marcelonfilho.ms@gmail.com>
2023-10-26 17:19:57 -0300

    fix: adjuste cart component

--- src/components/ui/cart.tsx
+++ src/components/ui/cart.tsx
@@ -1,22 +1,35 @@
-import { ShoppingCart } from "lucide-react";
+import { ShoppingCartIcon } from "lucide-react";
 import { Badge } from "./badge";
 import { useContext } from "react";
 import { CartContext } from "@/providers/cart";
 import CartItem from "./cart-item";
-import computeProductTotalPrice from "@/helpers/products";
+import { computeProductTotalPrice } from "@/helpers/products";
 import { Separator } from "./separator";
 import { ScrollArea } from "./scroll-area";
 import { Button } from "./button";
 import { createCheckout } from "@/actions/checkout";
 import { loadStripe } from "@stripe/stripe-js";
+import { createOrder } from "@/actions/order";
+import { useSession } from "next-auth/react";
 
 const Cart = () => {
+  const { data } = useSession();
+
   const { products, subtotal, total, totalDiscount } = useContext(CartContext);
 
   const handleFinishPurchaseClick = async () => {
-    const checkout = await createCheckout(products);
+    if (!data?.user) {
+      // TODO: redirecionar para o login
+      return;
+    }
+
+    const order = await createOrder(products, (data?.user as any).id);
+
+    const checkout = await createCheckout(products, order.id);
 
     const stripe = await loadStripe(process.env.NEXT_PUBLIC_STRIPE_PUBLIC_KEY);
+
+    // Criar pedido no banco
 
     stripe?.redirectToCheckout({
       sessionId: checkout.id,
@@ -25,29 +38,25 @@ const Cart = () => {
 
   return (
     <div className="flex h-full flex-col gap-8">
-      <Badge
-        className="w-fit gap-1 border-2 border-primary px-3 py-[0.375rem] text-xs uppercase"
-        variant="outline"
-      >
-        <ShoppingCart size={16} />
+      <Badge variant="heading">
+        <ShoppingCartIcon size={16} />
         Carrinho
       </Badge>
 
-      <div className="flex-1 overflow-hidden">
-        <ScrollArea>
-          <div className="flex flex-col gap-5">
+      {/* RENDERIZAR OS PRODUTOS */}
+      <div className="flex h-full max-h-full flex-col gap-5 overflow-hidden">
+        <ScrollArea className="h-full">
+          <div className="flex h-full flex-col gap-8">
             {products.length > 0 ? (
-              products.map((product) => {
-                return (
-                  <CartItem
-                    key={product.id}
-                    product={computeProductTotalPrice(product as any) as any}
-                  />
-                );
-              })
+              products.map((product) => (
+                <CartItem
+                  key={product.id}
+                  product={computeProductTotalPrice(product as any) as any}
+                />
+              ))
             ) : (
-              <p className="mt-8 text-center text-sm font-semibold opacity-75">
-                Carrinho vazio.
+              <p className="text-center font-semibold">
+                Carrinho vazio. Vamos fazer compras?
               </p>
             )}
           </div>
@@ -57,35 +66,36 @@ const Cart = () => {
       {products.length > 0 && (
         <div className="flex flex-col gap-3">
           <Separator />
-          <div className="flex items-center justify-between">
-            <p className="text-xs">Subtotal</p>
-            <p className="text-xs">R$ {subtotal.toFixed(2)}</p>
+
+          <div className="flex items-center justify-between text-xs">
+            <p>Subtotal</p>
+            <p>R$ {subtotal.toFixed(2)}</p>
           </div>
 
           <Separator />
 
-          <div className="flex items-center justify-between">
-            <p className="text-xs">Entrega</p>
-            <p className="text-xs uppercase">grátis</p>
+          <div className="flex items-center justify-between text-xs">
+            <p>Entrega</p>
+            <p>GRÁTIS</p>
           </div>
 
           <Separator />
 
-          <div className="flex items-center justify-between">
-            <p className="text-xs">Descontos</p>
-            <p className="text-xs">- R$ {totalDiscount.toFixed(2)}</p>
+          <div className="flex items-center justify-between text-xs">
+            <p>Descontos</p>
+            <p>- R$ {totalDiscount.toFixed(2)}</p>
           </div>
 
           <Separator />
 
-          <div className="flex items-center justify-between">
-            <p className="text-sm font-bold">Total</p>
-            <p className="text-sm font-bold">R$ {total.toFixed(2)}</p>
+          <div className="flex items-center justify-between text-sm font-bold">
+            <p>Total</p>
+            <p>R$ {total.toFixed(2)}</p>
           </div>
 
           <Button
-            onClick={handleFinishPurchaseClick}
             className="mt-7 font-bold uppercase"
+            onClick={handleFinishPurchaseClick}
           >
             Finalizar compra
           </Button>
