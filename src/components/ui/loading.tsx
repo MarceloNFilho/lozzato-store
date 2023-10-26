9074d19b7b08763d7842e9876ceeae4977dc031c
marcelonfilho <marcelonfilho.ms@gmail.com>
2023-10-26 18:24:08 -0300

    style: change skeleton size

--- src/components/ui/loading.tsx
+++ src/components/ui/loading.tsx
@@ -6,23 +6,23 @@ const LoadingPage = () => {
         <div className="grid grid-cols-2 gap-8">
           <div className="flex flex-col gap-4">
             <div className="h-44 w-44 rounded-lg bg-gray-300 opacity-50" />
-            <div className="h-8 w-32 rounded-lg bg-gray-300 opacity-50" />
-            <div className="h-8 w-24 rounded-lg bg-gray-300 opacity-50" />
+            <div className="h-6 w-32 rounded-lg bg-gray-300 opacity-50" />
+            <div className="h-6 w-24 rounded-lg bg-gray-300 opacity-50" />
           </div>
           <div className="flex flex-col gap-4">
             <div className="h-44 w-44 rounded-lg bg-gray-300 opacity-50" />
-            <div className="h-8 w-32 rounded-lg bg-gray-300 opacity-50" />
-            <div className="h-8 w-24 rounded-lg bg-gray-300 opacity-50" />
+            <div className="h-6 w-32 rounded-lg bg-gray-300 opacity-50" />
+            <div className="h-6 w-24 rounded-lg bg-gray-300 opacity-50" />
           </div>
           <div className="flex flex-col gap-4">
             <div className="h-44 w-44 rounded-lg bg-gray-300 opacity-50" />
-            <div className="h-8 w-32 rounded-lg bg-gray-300 opacity-50" />
-            <div className="h-8 w-24 rounded-lg bg-gray-300 opacity-50" />
+            <div className="h-6 w-32 rounded-lg bg-gray-300 opacity-50" />
+            <div className="h-6 w-24 rounded-lg bg-gray-300 opacity-50" />
           </div>
           <div className="flex flex-col gap-4">
             <div className="h-44 w-44 rounded-lg bg-gray-300 opacity-50" />
-            <div className="h-8 w-32 rounded-lg bg-gray-300 opacity-50" />
-            <div className="h-8 w-24 rounded-lg bg-gray-300 opacity-50" />
+            <div className="h-6 w-32 rounded-lg bg-gray-300 opacity-50" />
+            <div className="h-6 w-24 rounded-lg bg-gray-300 opacity-50" />
           </div>
         </div>
       </div>
