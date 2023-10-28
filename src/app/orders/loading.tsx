edb46f98f38ec4d74a3c8e59c3b8d1dd6fc8cd0c
marcelonfilho <marcelonfilho.ms@gmail.com>
2023-10-27 23:06:21 -0400

    feat: add responsiveness to loading in orders page

--- src/app/orders/loading.tsx
+++ src/app/orders/loading.tsx
@@ -2,7 +2,7 @@ import LoadingPage from "@/components/ui/loading";
 
 const Loading = () => {
   return (
-    <div className="flex w-full flex-1 flex-col p-5">
+    <div className="flex w-full max-w-[1280px] flex-1 flex-col p-5 lg:mx-auto">
       <div className="flex w-full animate-pulse flex-col gap-8">
         <div className="h-8 w-28 rounded-lg bg-accent opacity-75" />
         <div className="flex flex-col gap-4">
